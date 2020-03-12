f15e0d1f5cd4b1e07d865daf6bf79bd0f03b8ec4
Eric <eric.j.lee8@gmail.com>
2020-03-11 18:05:44 -0700

    Food Details Style changed
    Details of each food according to consistent style

--- js/food_popup.js
+++ js/food_popup.js
@@ -18,17 +18,23 @@ var w = 480, h = 340;
                             ',top=' + topPos + ',left=' + leftPos + ',       scrollbars=yes');
   
   msgWindow.document.write 
-      ('<HTML><HEAD><TITLE>Centered Window</TITLE></HEAD><BODY><FORM    NAME="form1">' +
-      '<img src="images/dumplings1.PNG" width=30%><img src="images/dumplings2.PNG" width=30%><img src="images/dumplings3.PNG" width=30%>'+
-      ' <H1>Food: Chicken Dumplings</H1>'+
+      ('<HTML><HEAD>'+
+      '<style>header{background-color: #DED2A8;padding: 0rem;}' +
+        'body {background-color:white;font-family: \'Open Sans\', sans-serif;padding: 2rem;}' +
+        'section{background-color:#DED2A9; padding: 2rem;}'+
+        '#reviewtext{margin-left: auto;margin-right: auto;}' +
+        '</style>'+
+      '<TITLE>Centered Window</TITLE></HEAD><BODY><FORM    NAME="form1">' +
+      '<img src="images/dumplings1.PNG" height=20%><img src="images/dumplings2.PNG" height=20%><img src="images/dumplings3.PNG" height=20%>'+
+      '<section> <H1>Food: Chicken Dumplings</H1>'+
       'Flour coating with Chicken and vegtable fillings cooked in our special house made spicy sauce.' +
       '<br>' +
       'Ingredients:'+
       '<br>' +
       '<ul><li>Chicken</li><li>Carrots</li><li>Spinich</li><li>Red chillies</li></ul>' +
       '<h3>Tell us how was your experience with this dish:</h3>' +
-      '<textarea id="w3mission" rows="4" cols="80"></textarea>' + '<br>' +
-      '<INPUT TYPE="button" VALUE="OK"onClick="window.close();"></FORM></BODY>   </HTML>');
+      '<textarea id="reviewtext" rows="4" cols="80"></textarea>' + '<br>' +
+      '<INPUT TYPE="button" VALUE="OK"onClick="window.close();"></section></FORM></BODY>   </HTML>');
   }
 
   function openWindow2(){
@@ -44,19 +50,25 @@ var w = 480, h = 340;
     
     
     
-    msgWindow = window.open('','popup','width=' + popW + ',height=' + popH + 
-                              ',top=' + topPos + ',left=' + leftPos + ',       scrollbars=yes');
-    
-    msgWindow.document.write 
-        ('<HTML><HEAD><TITLE>Centered Window</TITLE></HEAD><BODY><FORM    NAME="form1">' +
-        '<img src="images/rolls1.PNG" width=30%><img src="images/rolls2.PNG" width=30%><img src="images/rolls3.PNG" width=30%>'+
-        ' <H1>Food: Vegtable Spring Rolls</H1>'+
+    msgWindow = window.open('','popup','width=' + popW + ',height=' + popH + ',top=' + topPos + ',left=' + leftPos + ', scrollbars=yes');
+
+        msgWindow.document.write 
+        ('<HTML><HEAD>'+
+        '<style>header{background-color: #DED2A8;padding: 0rem;}' +
+        'body {background-color:white;font-family: \'Open Sans\', sans-serif;padding: 2rem;}' +
+        'section{background-color:#DED2A9; padding: 2rem;}'+
+        '#reviewtext{margin-left: auto;margin-right: auto;}' +
+        '</style>'+
+        '<TITLE>Centered Window</TITLE></HEAD><BODY><FORM    NAME="form1">' +
+        '<img src="images/rolls1.PNG" height=30%><img src="images/rolls2.PNG" height=30%><img src="images/rolls3.PNG" height=30%>'+
+        '<section> <H1>Food: Vegtable Spring Rolls</H1>'+
         'Deep Fried bread coating with mix vegtable fillings.' +
         '<br>' +
         'Ingredients:'+
         '<br>' +
         '<ul><li>Bamboo</li><li>Cabbage</li><li>Carrot</li><li>Mushroom</li></ul>' +
         '<h3>Tell us how was your experience with this dish:</h3>' +
-        '<textarea id="w3mission" rows="4" cols="80"></textarea>' + '<br>' +
-        '<INPUT TYPE="button" VALUE="OK"onClick="window.close();"></FORM></BODY>   </HTML>');
-    }
+                              '<textarea id="reviewtext" rows="4" cols="80"></textarea>' + '<br>' +
+                              '<INPUT TYPE="button" VALUE="OK"onClick="window.close();"></section></FORM></BODY>   </HTML>');
+                          }
+    
